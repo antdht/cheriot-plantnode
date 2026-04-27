6fabd077744eca81af925e18bfcfec70fa0c4a3d
antdht <antdht@proton.me>
2026-04-27 11:26:38 +0200

    fix: removed rand_32.cc

--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,9 @@
 
 set_project("CHERIoT smart plant node")
 
-sdkdir      = "../sonata-software/cheriot-rtos/sdk"
+sdkdir = "../sonata-software/cheriot-rtos/sdk"
 netstackdir = "../sonata-software/network-stack/lib"
-libsdir     = "../sonata-software/libraries"
+libsdir = "../sonata-software/libraries"
 includes(sdkdir)
 includes(netstackdir)
 includes(libsdir)
@@ -29,10 +29,8 @@ add_deps("freestanding", "debug")
 
 compartment("comms")
 add_files("src/comms.cc")
-add_includedirs("src", "../sonata-software/network-stack/include",
-                "../sonata-software/network-stack/examples/04.MQTT")
-add_deps("freestanding", "debug", "DNS", "TCPIP", "NetAPI", "TLS",
-         "Firewall", "SNTP", "MQTT", "time_helpers", "stdio")
+add_includedirs("src", "../sonata-software/network-stack/include", "../sonata-software/network-stack/examples/04.MQTT")
+add_deps("freestanding", "debug", "DNS", "TCPIP", "NetAPI", "TLS", "Firewall", "SNTP", "MQTT", "time_helpers", "stdio")
 add_rules("cheriot.network-stack.ipv6")
 
 compartment("data_processing")
@@ -54,7 +52,6 @@ add_deps("freestanding", "debug")
 compartment("crypto")
 add_files("src/crypto.cc")
 add_files("../cheriot-demos/third_party/crypto/libhydrogen/hydrogen.c")
-add_files("src/rand_32.cc")
 add_includedirs("src", "../cheriot-demos/third_party/crypto/libhydrogen")
 add_defines("CHERIOT_NO_AMBIENT_MALLOC")
 add_deps("freestanding", "debug")
@@ -86,8 +83,7 @@ add_deps("freestanding", "debug")
 firmware("plantnode")
 add_deps("freestanding", "debug")
 -- Application
-add_deps("core_logic", "comms", "data_processing", "policy_engine",
-         "attestation", "crypto", "display")
+add_deps("core_logic", "comms", "data_processing", "policy_engine", "attestation", "crypto", "display")
 -- Drivers
 add_deps("i2c_driver", "moisture_sensor", "temperature_sensor", "pump_driver")
 -- Network stack
@@ -95,13 +91,22 @@ add_deps("TCPIP", "TLS", "MQTT", "Firewall", "NetAPI", "DNS", "SNTP")
 -- Required for RSA certificate validation (mosquitto.org uses RSA)
 add_options("tls-rsa")
 after_link(function(target)
-	local fw     = target:targetfile()
+	local fw = target:targetfile()
 	local outdir = path.join(os.projectdir(), "build/uf2")
 	os.mkdir(outdir)
 	os.execv("llvm-strip", { fw, "-o", fw .. ".strip" })
-	os.execv("uf2conv", { fw .. ".strip", "-b0x00000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot1.uf2") })
-	os.execv("uf2conv", { fw .. ".strip", "-b0x10000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot2.uf2") })
-	os.execv("uf2conv", { fw .. ".strip", "-b0x20000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot3.uf2") })
+	os.execv(
+		"uf2conv",
+		{ fw .. ".strip", "-b0x00000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot1.uf2") }
+	)
+	os.execv(
+		"uf2conv",
+		{ fw .. ".strip", "-b0x10000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot2.uf2") }
+	)
+	os.execv(
+		"uf2conv",
+		{ fw .. ".strip", "-b0x20000000", "-f0x6CE29E60", "-co", path.join(outdir, "plantnode.slot3.uf2") }
+	)
 end)
 on_load(function(target)
 	target:values_set("board", "sonata-1.1")
@@ -109,24 +114,24 @@ on_load(function(target)
 		{
 			-- Main application thread. Stack must cover the full
 			-- cross-compartment call chain incl. TLS in comms.
-			compartment        = "core_logic",
-			priority           = 1,
-			entry_point        = "core_entry",
-			stack_size         = 8160,
+			compartment = "core_logic",
+			priority = 1,
+			entry_point = "core_entry",
+			stack_size = 8160,
 			trusted_stack_frames = 8,
 		},
 		{
-			compartment        = "TCPIP",
-			priority           = 1,
-			entry_point        = "ip_thread_entry",
-			stack_size         = 0x1000,
+			compartment = "TCPIP",
+			priority = 1,
+			entry_point = "ip_thread_entry",
+			stack_size = 0x1000,
 			trusted_stack_frames = 5,
 		},
 		{
-			compartment        = "Firewall",
-			priority           = 2,
-			entry_point        = "ethernet_run_driver",
-			stack_size         = 0x1000,
+			compartment = "Firewall",
+			priority = 2,
+			entry_point = "ethernet_run_driver",
+			stack_size = 0x1000,
 			trusted_stack_frames = 5,
 		},
 	}, { expand = false })
